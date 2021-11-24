d75c9bab82afb699b7be088939ea5ab840dd2197
MiniaczQ <MiniaczQ@users.noreply.github.com>
2021-11-24 13:15:53 +0100

    Complete proof of concept in Python

---
+++ src/db/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+database = create_engine('sqlite://', connect_args = {"check_same_thread": False})
+
+databaseSession = sessionmaker(autocommit=False, autoflush=False, bind=database)
+
+baseEntity = declarative_base()
