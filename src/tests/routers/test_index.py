57c4fab4e77b704f8c0203438a1759bd23d1f1de
MiniaczQ <MiniaczQ@users.noreply.github.com>
2021-12-01 20:37:10 +0100

    tests for user requests

--- src/tests/routers/test_index.py
+++ src/tests/routers/test_index.py
@@ -1,5 +1,8 @@
 from fastapi.testclient import TestClient
+from src.dependencies import get_db
+from sqlalchemy.orm import Session
+from fastapi import Depends
 
-def test_index(client: TestClient):
+def test_index(client: TestClient, db: Session = Depends(get_db)):
     response = client.get("/")
     assert response.status_code == 200
--- src/tests/routers/test_user.py
+++ src/tests/routers/test_user.py
@@ -1,5 +1,30 @@
 from fastapi.testclient import TestClient
+from src.dependencies import get_db
+from sqlalchemy.orm import Session
+from fastapi import Depends
 
-def test_index(client: TestClient):
-    response = client.get("/user/")
+def test_login(client: TestClient, db: Session = Depends(get_db)):
+    data = {
+        'e_mail': 'bartek@gmail.com',
+        'password': 'Bartek',
+    }
+    response = client.post("/user/login/", data)
+    assert response.status_code == 200
+
+def test_register(client: TestClient, db: Session = Depends(get_db)):
+    data = {
+        'e_mail': 'unique@email.com',
+        'password': 'balbinka',
+        'first_name': 'chedar',
+        'last_name': 'cheese',
+    }
+    response = client.post("/user/register/", data)
+    json = response.json()
+    for key in ['e_mail', 'first_name', 'last_name']:
+        assert json[key] == data[key]
+    assert json.get('user_id') is not None
+    assert response.status_code == 200
+
+def test_lookup(client: TestClient, db: Session = Depends(get_db)):
+    response = client.get("/user/lookup/1")
     assert response.status_code == 200
