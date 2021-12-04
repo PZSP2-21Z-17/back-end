121165ee382058e9de1a53211ab828000c583a95
MiniaczQ <MiniaczQ@users.noreply.github.com>
2021-12-04 18:00:21 +0100

    user requests error handling

--- src/routers/user.py
+++ src/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from src.dependencies import get_db
@@ -10,17 +10,30 @@ router = APIRouter()
 @router.post("/register/", response_model=UserLookup)
 def register(user: UserCreate, db: Session = Depends(get_db)):
     db_user = UserModel(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+    try:
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+    except Exception as error:
+        print(error)
+        db.rollback()
+        return HTTPException(status_code=404)
     return db_user
 
 @router.post("/login/", response_model=UserLookup)
 def login(user: UserLogin, db: Session = Depends(get_db)):
-    db_user = db.query(UserModel).filter(UserModel.password == user.password).filter(UserModel.e_mail == user.e_mail).one()
+    try:
+        db_user = db.query(UserModel).filter(UserModel.password == user.password).filter(UserModel.e_mail == user.e_mail).one()
+    except Exception as error:
+        print(error)
+        return HTTPException(status_code=404)
     return db_user
 
 @router.get("/lookup/{user_id}", response_model=UserLookup)
 def lookup(user_id: int, db: Session = Depends(get_db)):
-    db_user = db.query(UserModel).filter(UserModel.user_id == user_id).one()
+    try:
+        db_user = db.query(UserModel).filter(UserModel.user_id == user_id).one()
+    except Exception as error:
+        print(error)
+        return HTTPException(status_code=404)
     return db_user
