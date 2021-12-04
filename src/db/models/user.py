d09c8ece1de2a6a19ab2d1bd0792441ac425887a
MiniaczQ <MiniaczQ@users.noreply.github.com>
2021-12-04 16:05:59 +0100

    missed one D:

--- src/db/models/user.py
+++ src/db/models/user.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column
-from sqlalchemy.orm import relation, relationship
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import Integer, Text
-from src.db.database import BaseModel
 
+from src.db.database import BaseModel
 
 class User(BaseModel):
     __tablename__   = 'user'
