9df4a423c40b4a2cdc275414098717f1d824133d
MiniaczQ <MiniaczQ@users.noreply.github.com>
2021-12-04 16:13:06 +0100

    included all basic routers

--- src/app.py
+++ src/app.py
@@ -4,15 +4,22 @@ from fastapi.middleware.cors import CORSMiddleware
 from .db.models import *
 from .db.database import BaseModel, database
 from .db import dummy
-from .routers import user, group, tag
+from .routers import *
 
 BaseModel.metadata.create_all(bind=database)
 dummy.fill()
 
 app = FastAPI()
-app.include_router(user.router, prefix="/user")
+app.include_router(answer.router, prefix="/answer")
+app.include_router(exam.router, prefix="/exam")
 app.include_router(group.router, prefix="/group")
+app.include_router(subject.router, prefix="/subject")
+app.include_router(tag_aff.router, prefix="/tag_aff")
 app.include_router(tag.router, prefix="/tag")
+app.include_router(task_aff.router, prefix="/task_aff")
+app.include_router(task.router, prefix="/task")
+app.include_router(user_aff.router, prefix="/user_aff")
+app.include_router(user.router, prefix="/user")
 
 app.add_middleware(
     CORSMiddleware,
