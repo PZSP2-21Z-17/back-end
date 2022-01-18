1b401b2b114d3132e2e0a53da9656af8045141f8
MiniaczQ <MiniaczQ@users.noreply.github.com>
2022-01-18 22:59:46 +0100

    fixed error handling?

--- src/routers/exceptions.py
+++ src/routers/exceptions.py
@@ -3,4 +3,4 @@ from http.client import HTTPException
 from fastapi import status
 
 def HTTPUnauthorized() -> HTTPException:
-    HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
