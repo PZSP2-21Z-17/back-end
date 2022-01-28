d0973741940c702b4f986b3f2c138de5af33f9f4
Dove6 <Dove6@users.noreply.github.com>
2022-01-28 11:40:21 +0100

    Add GCP IP to CORS

--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ app.include_router(user.router, prefix="/user")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://34.118.106.233:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
